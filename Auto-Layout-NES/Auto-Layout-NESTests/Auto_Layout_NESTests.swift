51cd1af93f92c96f691617ba1760b2cd5ecca6c1
Nate Spry <nspry1997@gmail.com>
2021-11-03 18:56:14 -0400

    Add files via upload

---
+++ Auto-Layout-NES/Auto-Layout-NESTests/Auto_Layout_NESTests.swift
@@ -0,0 +1,33 @@
+//
+//  Auto_Layout_NESTests.swift
+//  Auto-Layout-NESTests
+//
+//  Created by Nathaniel Spry on 11/3/21.
+//
+
+import XCTest
+@testable import Auto_Layout_NES
+
+class Auto_Layout_NESTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
